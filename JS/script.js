1351a2ad99acfc5ba80a745079a65b40ef2fe880
Pingbo Yang <86988793+PingboYang@users.noreply.github.com>
2022-10-13 20:41:14 -0400

    Add taskManger file and finished task 4 and 5.

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,67 @@
-function validFormFieldInput(e){
+const taskManager = new TaskManager();
+
+function addTask(e){
+
+    const newName=document.querySelector('#name');
+    const nameError=document.querySelector('#name-error');
+    const hasName=requireField(newName,nameError);
+
+    const newDescription=document.querySelector('#description');
+    const descriptionError=document.querySelector('#description-error');
+    const hasDescription=requireField(newDescription,descriptionError);
+
+    const newAssigned=document.querySelector('#assigned');
+    const assignedError=document.querySelector('#assigned-error');
+    const hasAssigned=requireField(newAssigned,assignedError);
     
-    e.preventDefault()
+    const newDueDate=document.querySelector('#dueDate');
+    const dueDateError=document.querySelector('#dueDate-error');
+    const hasDueDate=requireField(newDueDate,dueDateError);
+
+    const newStatus=document.querySelector('#status');
+    const statusError=document.querySelector('#status-error');
+    const hasStatus=requireField(newDueDate,dueDateError);
     
-    const newName = document.querySelector('#name');
-    const name = newName.value;
-    console.log(name);
+    const isValid = hasName && hasDescription && hasAssigned && hasDueDate && hasStatus;
+    if(isValid){
+        taskManager.addTask(newName.value, newDescription.value, newAssigned.value, newDueDate.value, newStatus.value);
+
+    }
+   
+    newName.value="";
+    newDescription.value="";
+    newAssigned.value="";
+    newDueDate.value="";
+    newStatus.value=""
+
+    
+
 }
+
+function requireField(field, errorField){
+    const value = field.value;
+    console.log(value);
+   
+    
+    if (value) {
+        errorField.classList.add("invisible");
+        return true;
+    } else {
+
+            errorField.classList.remove("invisible");
+            return false;
+         }
+}
+
+
 const button = document.getElementById('button');
-button.onclick = validFormFieldInput
+button.onclick = addTask;
+console.log("Hello");
+
+
+console.log(taskManager.tasks);
+
+console.log(taskManager.currentId);
+
+
+
---
+++ JS/taskManager.js
@@ -0,0 +1,19 @@
+class TaskManager{
+    constructor(currentId=0) {
+        this.currentId=currentId;
+        this.tasks=[];
+    }
+
+    addTask(name,description,assignedTo,dueDate,studes){
+        this.currentId=this.currentId+1;
+        const task = {
+            id: this.currentId,
+            name: name,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: 'TODO'
+        }; 
+        this.tasks.push(task);
+    }
+}
