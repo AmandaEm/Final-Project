a7b7db94ef9023218e8d01be4f20666216c52eca
tesfaye <tesfayewaktola2021@gmail.com>
2022-10-16 17:13:22 -0500

    I removed the declaration of variables since it is the parameter of function(correct on console error)

--- JS/taskManager.js
+++ JS/taskManager.js
@@ -1,12 +1,11 @@
 function createTaskHtml (name, description, assignedTo, dueDate, _status, textarea) {
-    const name = `<div class="form-group">
+   name = `<div class="form-group">
     <label for="exampleFormControlInput1">Name</label>
     <input type="text" class="form-control" id="name" placeholder="Name">
     <div id="name-error" class="alert alert-danger invisible"  role="alert">
       Input your name!
     </div>`;
-
-    const description = `<div class="form-group">
+   description = `<div class="form-group">
     <label for="exampleFormControlSelect1">Description</label>
     <input type="text" class="form-control" id="description" placeholder="Description">
     <div id="description-error" class="alert alert-danger invisible"  role="alert">
@@ -14,21 +13,21 @@ function createTaskHtml (name, description, assignedTo, dueDate, _status, textar
     </div>
   </div>`;
 
-   const assignedTo = `<div class="form-group">
+  assignedTo = `<div class="form-group">
    <label for="exampleFormControlSelect2">Assigned To</label>
    <input type="text" class="form-control" id="assigned" placeholder="Assigned to">
    <div id="assigned-error" class="alert alert-danger invisible"  role="alert">
      Input who you want to assigned to! 
    </div>
  </div>`;
-   const dueDate = `<div class="form-group">
+   dueDate = `<div class="form-group">
    <label for="exampleFormControlSelect2">Due Date</label>
    <input type="text" class="form-control" id="dueDate" placeholder="Due Date">
    <div id="dueDate-error" class="alert alert-danger invisible"  role="alert">
      Input your due date!
    </div>
  </div>`;
-   const _status = `<div class="form-group">
+   _status = `<div class="form-group">
    <label for="exampleFormControlSelect1">Status</label>
    <select class="form-control" id="_status" placeholder="Status">
    <div id="status-error" class="alert alert-danger invisible"  role="alert">
@@ -40,7 +39,7 @@ function createTaskHtml (name, description, assignedTo, dueDate, _status, textar
      </select>
    </div>
    </div>`;
-   const textarea = `</div>
+    textarea = `</div>
    <div class="form-group">
      <label for="exampleFormControlTextarea1">Description</label>
      <textarea class="form-control" id="textarea" rows="3"></textarea>
