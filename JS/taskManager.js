268d479bf9b342a748ab96ab6ccb2fecfec468e8
Vanessa Diaz <108776281+van4444@users.noreply.github.com>
2022-10-16 21:29:14 -0500

    updated task six

--- JS/taskManager.js
+++ JS/taskManager.js
@@ -49,7 +49,10 @@ function createTaskHtml (name, description, assignedTo, dueDate, _status, textar
    return html 
    const taskHtml = createTaskHtml('Tim', 'Wash dishes', 'Tim', '10/31/2022', 'In Progress')
    console.log(taskHtml);
-   
+   render
+   const tasksHtmlList[];
+
+
 }
 
 
